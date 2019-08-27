537a1b125964df5050048d15f3980e30faecb5a9
Sheng <Sheng@DESKTOP-4E4OQR0>
2019-08-27 14:47:43 +0800

    1

---
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -0,0 +1,17 @@
+package com.tedu.sp01.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors
+public class Item {
+
+	private Integer id;		//商品id	
+	private String name; 	//商品名称
+	private Integer number;	//商品库存
+}
